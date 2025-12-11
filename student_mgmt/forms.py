8fdc7f896ce9b6085a0bf7c49b942b3a790ff829
Ashif EK <ashifek11@gmail.com>
2025-12-11 20:43:56 +0530

    feat: Revamp student forms and list view with enhanced fields and layout

--- student_mgmt/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from accounts.models import StudentProfile
-
-class StudentProfileForm(forms.ModelForm):
-    class Meta:
-        model = StudentProfile
-        fields = ['roll_number', 'department', 'year_of_admission']
