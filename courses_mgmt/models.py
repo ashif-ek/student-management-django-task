d8208f76a8afffc8ed58b654cf11595a3b72a15b
Ashif EK <ashifek11@gmail.com>
2025-12-12 12:09:01 +0530

    feat: Add Course and Enrollment models, including completion status fields for enrollment.

--- courses_mgmt/models.py
+++ courses_mgmt/models.py
@@ -3,13 +3,9 @@ from accounts.models import CustomUser
 
 
 class Course(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=255)
     description = models.TextField()
-    thumbnail = models.ImageField(
-        upload_to='course_thumbnails/',
-        blank=True,
-        null=True
-    )
+    thumbnail = models.ImageField(upload_to="course_thumbnails/", blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -17,11 +13,13 @@ class Course(models.Model):
         return self.title
 
 
-
 class Enrollment(models.Model):
     student = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
     assigned_at = models.DateTimeField(auto_now_add=True)
 
+    completed = models.BooleanField(default=False)
+    completed_at = models.DateTimeField(null=True, blank=True)
+
     def __str__(self):
-        return f"{self.student.username} → {self.course.title}"
+        return f"{self.student.username} -> {self.course.title}"
