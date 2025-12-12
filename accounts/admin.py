7d44265fcb38c09b023417c80361aa2c5ef6862f
Ashif EK <ashifek11@gmail.com>
2025-12-12 11:53:45 +0530

    feat: Add Django admin configurations for courses and accounts models, and implement a login page.

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser, StudentProfile
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ['username', 'email', 'role', 'is_staff', 'is_active']
+    list_filter = ['role', 'is_staff', 'is_active']
+    fieldsets = UserAdmin.fieldsets + (
+        ('Role Information', {'fields': ('role',)}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Role Information', {'fields': ('role',)}),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(StudentProfile)
